d80ecbfe5556abfe32b88da1d01c80e28c5fde1c
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-06-02 10:46:13 +0530

    Add root url

--- src/main/java/com/services/resourceallocation/controller/UserController.java
+++ src/main/java/com/services/resourceallocation/controller/UserController.java
@@ -34,6 +34,11 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/")
+    public String home() {
+        return "Resource Allocation Backend Running Successfully";
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
        try {
